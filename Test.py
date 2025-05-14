0203697d1b10d9c4e548ec22b50581ec816b290f
Mad0 <jose.antonio.pepe@icloud.com>
2025-05-13 20:14:52 -0600

    test 1

--- Test.py
+++ Test.py
@@ -2,9 +2,10 @@ import cv2
 import Tracking
 import Transformation
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 frame_tracking = None
 frame_blank = None
+transformation = None
 
 if not cap.isOpened():
     print("Error: no se pudo abrir la cámara.")
@@ -17,27 +18,32 @@ while True:
         break
 
     # 1) Detección de todos los ArUco
-    coordenadas, frame_aruco, corners, ids = Transformation.detectar_aruco(frame, True)
-    cv2.imshow("Deteccion aruco", frame)
+    coordenadas, frame_aruco, corners, ids = Transformation.detectar_aruco(frame, False)
+    cv2.imshow("Deteccion aruco", frame_aruco)
 
     # Inicializa imagen_transformada
     imagen_transformada = None
 
     # 2) Transformación de perspectiva solo si tenemos al menos 4 esquinas
-    if coordenadas:
-        imagen_transformada = Transformation.transformar_perspectiva(frame, coordenadas, corners)
-        if imagen_transformada is not None and imagen_transformada.size > 0:
-            cv2.imshow("Imagen transformada con perspectiva", imagen_transformada)
-            print("Transformar perspectiva")
-        else:
-            print("Error: la transformación de perspectiva falló.")
-            imagen_transformada = frame_aruco
+    # Para cambiar perspectiva volver a poner None en transformation
+    if coordenadas and transformation is None:
+        transformation = Transformation.obtener_perspectiva(coordenadas)
+        print("Transformacion establecida")
+    elif transformation is None:
+        print("Error: no se pudo obtener la perspectiva")
+
+
+    imagen_transformada = Transformation.aplicar_perspectiva(frame_aruco, transformation)
+
+    if transformation is not None and imagen_transformada is not None:
+        cv2.imshow("Imagen transformada", imagen_transformada)
     else:
-        print("No se detectaron suficientes coordenadas ArUco.")
+        print("Error: no se pudo aplicar la perspectiva")
         imagen_transformada = frame_aruco
 
+
     # 3) Detección del ArUco específico en la imagen transformada
-    coordenadas_id, frame_con_id, corners_id, ids_id = Transformation.detectar_aruco_id(imagen_transformada, 0, True)
+    coordenadas_id, frame_con_id, corners_id, ids_id = Transformation.detectar_aruco_id(imagen_transformada, 0, False)
     cv2.imshow("Deteccion aruco con id 0", frame_con_id)
 
     # 4) Tracking — aquí Tracking.tracking devolverá siempre dos frames válidos
